b540614a2d9337c7f33e4adff3fff2dfbb90a2ca
RobertZakoyan <114802387+RobertZakoyan@users.noreply.github.com>
2024-02-11 01:22:53 +0400

    Update BubbleSort.py

--- First_Homework/BubbleSort.py
+++ First_Homework/BubbleSort.py
@@ -1,7 +1,11 @@
-def BuubleSort(arr):
+def BubbleSort(arr):
+    swapped = False 
     for i in range(len(arr)):
         for j in range(len(arr)-1):
             if arr[j] > arr[j+1]:
+                swapped = True
                 arr[j+1], arr[j] = arr[j], arr[j+1]
+        if not swapped:
+            break
     return arr
-print(BuubleSort([4,1,4,1,888,0,0,-1]))
+print(BubbleSort([4,1,4,1,888,0,0,-1]))
