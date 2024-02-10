3788ab08f5150956866a744ff39a65beee994acf
RobertZakoyan <114802387+RobertZakoyan@users.noreply.github.com>
2024-02-11 01:25:53 +0400

    Update SelectionSort.py

--- First_Homework/SelectionSort.py
+++ First_Homework/SelectionSort.py
@@ -1,9 +1,10 @@
 def selectionSort(array):
-    for index in range(len(array)):
+    for index in range(len(array)-1):
         min_index = index
         for j in  range(index+1, len(array)):
             if array[min_index] > array[j]:
                 min_index = j
-        array[min_index], array[index] = array[index], array[min_index] 
+        if min_index != index: 
+            array[min_index], array[index] = array[index], array[min_index] 
     return array
 print(selectionSort([1,4,4,6,0,-1]))
